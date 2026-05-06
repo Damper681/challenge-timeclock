301d4655f7b9c07fe7405317073566f28db71b15
Damper681 <perroud.damien@gmail.com>
2026-05-06 08:57:49 +0200

    Update firebase.js

--- challenge-timeclock/challenge-timeclock/src/firebase.js
+++ challenge-timeclock/challenge-timeclock/src/firebase.js
@@ -3,8 +3,7 @@
 // ============================================================
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
-
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyDT8yszAlNCeOn8gLNvfF2-n-MMh_4TL_g",
   authDomain: "challenge-atelier.firebaseapp.com",
@@ -13,7 +12,7 @@ const firebaseConfig = {
   messagingSenderId: "1096646029782",
   appId: "1:1096646029782:web:edbadee6ed93392f1ff477"
 }
-
+ 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
-export const storage = getStorage(app)
+ 
